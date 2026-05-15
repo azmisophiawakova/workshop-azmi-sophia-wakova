ddb80f4704d7a92f9cd22e17d53fab9908f983f5
akbar127 <fatwabitha@gmail.com>
2026-05-15 22:12:47 +0700

    git commit -m “menambahkan service.py Fatwabith-Akbar”

---
+++ src/Kelompok2-CommitSquad/Fatwabith-Akbar/service.py
@@ -0,0 +1,12 @@
+# Program menghitung luas lingkaran
+import math
+
+# Input nilai
+jari_jari = 7
+
+# Rumus luas lingkaran
+luas = math.pi * jari_jari * jari_jari
+
+# Output hasil
+print("Jari-jari =", jari_jari)
+print("Luas lingkaran =", luas)
