c6cfe0d85a4b5ad582c174d5db3019480935e7c6
Reymasst <rehananugrah598@gmail.com>
2026-05-15 22:49:21 +0700

    git commit -m “menambahkan service.py Raihan-Anugrah-Adrian-Arji”

---
+++ src/Kelompok2-CommitSquad/Raihan-Anugrah-Adrian-Arji/service.py
@@ -0,0 +1,13 @@
+# Program menghitung luas jajar genjang
+
+# Input nilai
+alas = 15
+tinggi = 8
+
+# Rumus luas jajar genjang
+luas = alas * tinggi
+
+# Output hasil
+print("Alas =", alas)
+print("Tinggi =", tinggi)
+print("Luas jajar genjang =", luas)
