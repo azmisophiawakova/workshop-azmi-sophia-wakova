ec3b25569dc87463b99d5ee1f9b49a87c46958cc
azmisophiawakova <azmisophiawakova@gmail.com>
2026-05-15 21:56:35 +0700

    git commit -m “menambahkan service.py Azmi Sophia Wakova”

---
+++ src/Kelompok2-CommitSquad/Azmi-Sophia-Wakova/service.py
@@ -0,0 +1,15 @@
+# Program menghitung luas trapesium
+
+# Input nilai
+sisi_atas = 10
+sisi_bawah = 14
+tinggi = 8
+
+# Rumus luas trapesium
+luas = 1/2 * (sisi_atas + sisi_bawah) * tinggi
+
+# Output hasil
+print("Sisi atas =", sisi_atas)
+print("Sisi bawah =", sisi_bawah)
+print("Tinggi =", tinggi)
+print("Luas trapesium =", luas)
