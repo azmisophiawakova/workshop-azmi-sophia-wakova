328b80789b32f53267234184118e5f1b0c905114
azmisophiawakova <azmisophiawakova@gmail.com>
2026-05-15 22:02:09 +0700

    git commit -m “menambahkan service.py Ajeng-Saskia-Maharani”

---
+++ src/Kelompok2-CommitSquad/Ajeng-Saskia-Maharani/service.py
@@ -0,0 +1,13 @@
+# Program menghitung luas segitiga
+
+# Input nilai
+alas = 12
+tinggi = 6
+
+# Rumus luas segitiga
+luas = 1/2 * alas * tinggi
+
+# Output hasil
+print("Alas =", alas)
+print("Tinggi =", tinggi)
+print("Luas segitiga =", luas)
